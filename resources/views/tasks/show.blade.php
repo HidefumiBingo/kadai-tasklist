2de0230328a0cf178fb52816b57d5d15e49084e5
hi-bing <bingo112881@gmail.com>
2021-06-23 07:28:28 +0000

    kadai-tasklist

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+
+    <h1>id = {{ $task->id }}のタスク詳細</h1>
+    
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <th>{{ $task->id }}</th>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    
+    {{-- 編集画面リンク --}}
+    {!! link_to_route('tasks.edit','タスクの編集',[$task->id],['class'=>'btn btn-light']) !!}
+    
+    {{-- タスク完了ボタン --}}
+    {!! Form::model($task,['route'=>['tasks.destroy',$task->id],'method'=>'delete']) !!}
+        {!! Form::submit('完了',['class'=>'btn btn-danger']) !!}
+    {!! Form::close() !!}
+
+
+@endsection
